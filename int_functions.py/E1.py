edf736620b9c1e22551f568fb2fa3bef63d93518
Agustín <82812682+AgusM01@users.noreply.github.com>
2021-08-24 18:33:03 -0300

    Update E1.py

--- int_functions.py/E1.py
+++ int_functions.py/E1.py
@@ -3,17 +3,17 @@
 #Imprimir los primeros 25 números naturales pares
 # naturales25: string -> string
 #No toma ningun valor
-#Devuelve los primeros 25 naturales
+#Devuelve los primeros 25 naturales pares
 # entrada: none, salida: "2, 4, 6, ...., 24"
 
-def naturales25 (n = 25):
-    if 0 == n:
-        print ("Fin")
+def naturales25 (n = 0, contador = 25):
+    if 0 == contador:
+        return
     else:
         if (n % 2) == 0:
             print (n)
-            naturales25 (n - 1)
+            naturales25 (n + 1, contador - 1)
         else:
-            naturales25 (n - 1)
+            naturales25 (n + 1, contador)
 
 naturales25()
